33b7edc4f2d9f5466edd599233fbbdddb48500b3
Maharoz <mahruj66@gmail.com>
2022-09-09 00:24:07 +0600

    mini max problem solved

---
+++ mini-max/mini-max/Program.cs
@@ -0,0 +1,17 @@
+﻿//https://www.hackerrank.com/challenges/mini-max-sum/
+
+string s = Console.ReadLine();
+long[] x = Array.ConvertAll(s.Split(' '), long.Parse);
+
+List<long> maxLst = x.OfType<long>().ToList();
+List<long> minLst = x.OfType<long>().ToList();
+
+long max = x.Max();
+long min = x.Min();
+
+maxLst.Remove(min);
+minLst.Remove(max);
+
+long maxtotal = maxLst.Sum(x => Convert.ToInt64(x));
+long mintotal = minLst.Sum(x => Convert.ToInt64(x));
+Console.WriteLine( mintotal+ " " + maxtotal);
