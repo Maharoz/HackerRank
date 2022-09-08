6430f7c8234bfe83eb683eccb219dab4edd1b04b
Maharoz <mahruj66@gmail.com>
2022-09-08 22:11:05 +0600

    First 3 Problem solved

---
+++ CompareTheTriplets/CompareTheTriplets/Program.cs
@@ -0,0 +1,26 @@
+﻿string aliceInput = Console.ReadLine();
+string bonInput = Console.ReadLine();
+
+int[] aliceArray = Array.ConvertAll(aliceInput.Split(' ').ToArray(), int.Parse);
+int[] bobArray = Array.ConvertAll(bonInput.Split(' ').ToArray(), int.Parse);
+
+int ptsForAlice = 0;
+int ptsForBob = 0;
+
+
+for(int i = 0; i < aliceArray.Length; i++)
+{
+    if(aliceArray[i] > bobArray[i])
+    {
+        ptsForAlice += 1;
+    }
+    else if(aliceArray[i] < bobArray[i])
+    {
+        ptsForBob += 1;
+    }
+}
+
+int[] res = { ptsForAlice, ptsForBob };
+
+Console.WriteLine(res[0]+" "+ res[1]);
+
---
+++ SimpleSumArray/SimpleSumArray/Program.cs
@@ -0,0 +1,16 @@
+﻿//https://www.hackerrank.com/challenges/simple-array-sum/problem
+int n = Convert.ToInt32(Console.ReadLine());
+
+string s = Console.ReadLine();
+
+string[] a = s.Split(' ').ToArray();
+
+int[] inputArr = Array.ConvertAll(a, int.Parse);
+
+int result = 0;
+
+foreach (int a2 in inputArr)
+{
+    result += a2;
+}
+Console.WriteLine(result);
