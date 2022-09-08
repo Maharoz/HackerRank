abf7fd2771f82874c600a10f6f64de841103b275
Maharoz <mahruj66@gmail.com>
2022-09-09 01:14:02 +0600

    birthday cake candle problem solved solved

---
+++ birthday-cake-candles/birthday-cake-candles/Program.cs
@@ -0,0 +1,20 @@
+﻿//https://www.hackerrank.com/challenges/birthday-cake-candles/
+int x = Convert.ToInt32(Console.ReadLine());
+int[] ar = Array.ConvertAll(Console.ReadLine().Split(' ').ToArray(), int.Parse);
+int max = ar[0];
+var occurrenceCount = 1;
+
+for (var i = 1; i < ar.Length; i++)
+{
+    if (ar[i] > max)
+    {
+        max = ar[i];
+        occurrenceCount = 1;
+    }
+    else if (ar[i] == max)
+    {
+        occurrenceCount++;
+    }
+}
+
+Console.WriteLine(occurrenceCount);
