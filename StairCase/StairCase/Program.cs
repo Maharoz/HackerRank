7ecffd88687bcb2ae0d6467fb36cf98da40555df
Maharoz <mahruj66@gmail.com>
2022-09-08 23:51:54 +0600

    stair case problem solved

---
+++ StairCase/StairCase/Program.cs
@@ -0,0 +1,10 @@
+﻿//https://www.hackerrank.com/challenges/staircase/
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+
+for(int i = 1; i <= n; i++)
+{
+    Console.WriteLine(new String(' ', n-i)+new string('#',i));
+}
