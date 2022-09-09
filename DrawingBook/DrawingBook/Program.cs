456f602f32365634b03f7c0286f4f15a42bb186f
Maharoz <mahruj66@gmail.com>
2022-09-10 02:28:33 +0600

    drawing book solved

--- DrawingBook/DrawingBook/Program.cs
+++ DrawingBook/DrawingBook/Program.cs
@@ -3,4 +3,4 @@
 int x = Convert.ToInt32(Console.ReadLine());
 int y= Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine(Math.Min(y/2,(x-y)/2));
+Console.WriteLine(Math.Min(y/2,((x/2)-(y/2))));
