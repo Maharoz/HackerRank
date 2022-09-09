e124b8c411290bd09a891a490a3cc5d6b84eaa4c
Maharoz <mahruj66@gmail.com>
2022-09-10 02:18:16 +0600

    drawing book solved

---
+++ DrawingBook/DrawingBook/Program.cs
@@ -0,0 +1,6 @@
+﻿//https://www.hackerrank.com/challenges/drawing-book/
+
+int x = Convert.ToInt32(Console.ReadLine());
+int y= Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Math.Min(y/2,(x-y)/2));
