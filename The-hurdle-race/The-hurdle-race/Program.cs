a03da3d981be4b63475de6dc79bbd2212cd9a2f1
Maharoz <mahruj66@gmail.com>
2022-09-11 01:17:31 +0600

    the hurdel race problem solved

---
+++ The-hurdle-race/The-hurdle-race/Program.cs
@@ -0,0 +1,14 @@
+﻿//https://www.hackerrank.com/challenges/the-hurdle-race/
+int[] input = Array.ConvertAll(Console.ReadLine().Split(' ').ToArray(), int.Parse);
+int[] hurdles = Array.ConvertAll(Console.ReadLine().Split(' ').ToArray(), int.Parse);
+
+int maxHurdle = hurdles.Max();
+
+if(maxHurdle> input[1])
+{
+    Console.WriteLine(maxHurdle - input[1]);
+}
+else
+{
+    Console.WriteLine(0);
+}
